4d271e6f2b61702ce0fa59001a3dd711e07a5fe8
gustav <gustav.segerstedt@gmail.com>
2023-08-05 16:22:35 +0200

    fixed small issue of login form scaling on mobile

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -14,7 +14,7 @@ const Login: React.FC<LoginProps> = () => {
   return (
     <div className="flex h-screen items-center justify-center">
       <div className="flex flex-col rounded-md p-4 shadow-md">
-        <Tabs defaultValue="Login" className="w-[400px]">
+        <Tabs defaultValue="Login">
           <TabsList>
             <TabsTrigger value="Login">Login</TabsTrigger>
             <TabsTrigger value="Signup">Signup</TabsTrigger>
