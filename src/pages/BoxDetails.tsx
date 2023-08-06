e812cf5583b519aa4c3211b57531285dc4853e94
gustav <gustav.segerstedt@gmail.com>
2023-08-06 16:15:54 +0200

    updated box details page

--- src/pages/BoxDetails.tsx
+++ src/pages/BoxDetails.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Layout from "../components/Layout";
-import { useNavigate, useParams } from "react-router-dom";
-import { Button } from "../components/ui/button";
+import { useParams } from "react-router-dom";
 import { Box, useGetSpecificBoxQuery } from "../generated/graphql";
-import EditBoxModal from "../components/EditBoxModal";
+import BoxCard from "../components/BoxCard";
 
 type BoxDetailsProps = {};
 
 const BoxDetails: React.FC<BoxDetailsProps> = () => {
-  const nav = useNavigate();
   const { id } = useParams();
   const { data, loading, error } = useGetSpecificBoxQuery({
     variables: {
@@ -27,34 +25,7 @@ const BoxDetails: React.FC<BoxDetailsProps> = () => {
 
   return (
     <Layout>
-      <div className="mt-10  flex w-10/12 rounded-md bg-white p-2">
-        <div className="flex w-full items-center gap-4 p-2">
-          {data && data.me && (
-            <>
-              <div className="flex flex-col">
-                <h1 className="text-lg text-gray-700">
-                  {data.me.boxes![0]!.name}
-                </h1>
-                <p className="text-sm text-gray-500">
-                  {data.me.boxes![0]!.description}
-                </p>
-                <p className="text-sm text-gray-500">
-                  {data.me?.boxes![0]?.items?.length}
-                </p>
-              </div>
-            </>
-          )}
-        </div>
-        <div className="flex w-full items-end justify-end">
-          <EditBoxModal box={data?.me?.boxes![0] as Box} />
-        </div>
-      </div>
-
-      <div className="flex w-10/12 items-start justify-start">
-        <Button className="my-4 bg-gray-500 " onClick={() => nav(-1)}>
-          Back
-        </Button>
-      </div>
+      <BoxCard box={data?.me?.boxes![0] as Box} />
     </Layout>
   );
 };
